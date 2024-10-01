0fd41699c5d5020812e665850e19a96d10cd7893
NAUGHTYDEVIL <87941234+NAUGHTYDEVlL@users.noreply.github.com>
2024-10-01 12:59:49 -0400

    3

--- FXServer/txData/CFXDefaultFiveM_FA288E.base/resources/[mlo]/cfx-gabz-pillbox/fxmanifest.lua
+++ FXServer/txData/CFXDefaultFiveM_FA288E.base/resources/[mlo]/cfx-gabz-pillbox/fxmanifest.lua
@@ -1,8 +1,36 @@
-fx_version 'cerulean'
+fx_version 'adamant'
 game 'gta5'
 
-author 'GABZ'
-description 'PILLBOX'
-version '2.0.0'
-
 this_is_a_map 'yes'
+
+file 'gabz_timecycle_mods_1.xml'
+data_file 'TIMECYCLEMOD_FILE' 'gabz_timecycle_mods_1.xml'
+
+client_script {
+  "main.lua"
+}
+
+
+
+
+
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
+---rare5m.co - https://discord.gg/97hgBVHCvC--
